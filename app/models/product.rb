0112222419e7f0503507cf9331a2c3989430ceeb
girllogic <lillianlovich@yahoo.com>
2016-02-29 10:29:11 -0600

    added validations to schema, model, form

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,14 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :brand
 
+#by using brand and category instead of brand_id, category_id, we are making sure that there is a brand. category object present
+ validates_presence_of :name, :current_price, :quantity, :description, :brand, :category
+
+ #want price to be 0 or positive float
+ validates_ numericality_of :current_price, greater_than_or_equal_to: 0.01
+
+#want quantity to be greater
+ validates_ numericality_of :quantity, greater_than_or_equal_to: 0
 
   has_attached_file :avatar,
     styles: { large: "500x500#", medium: "300x300#", thumb: "100x100#" },
