7d2954fa732901c51488ba63cf3f8b6bd8c0c614
girllogic <lillianlovich@yahoo.com>
2016-03-01 10:39:33 -0600

    added search box for products in shop

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,7 +1,13 @@
 class StorefrontController < ApplicationController
   def index
+    if params[:search]
+      @products = Product.where("name LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+      if @products.empty?
+        flash.now[:notice] = "No result found for \" #{params[:search]} \", showing all products"
+        @products = Product.all
+      end
 
-  if params[:category_id]
+    elsif params[:category_id]
       @category = Category.find(params[:category_id])
       @products = Product.where(category_id: @category)
     else
